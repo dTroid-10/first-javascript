4ccc5a7f9cf28ca8a7323c0c0b042582876d0efe
dTroid-10 <darshanchikorde20@gmail.com>
2025-12-24 07:30:16 +0000

    container config done

---
+++ basics/dataType.js
@@ -0,0 +1,13 @@
+let age = 18    //integer
+let name = "Darshan"  //string
+let roll = null     //null
+let subject = undefined //undefined
+let avail = true    //boolean
+let experience = 2.7
+let grade = 'A'
+
+console.table([age,name,roll,subject,avail,experience,grade])
+console.table([typeof age,typeof name,typeof roll,typeof subject,typeof avail,typeof experience,typeof grade])
+
+// the datatype of null is object whereas the datatype of undefined is undefined itself
+
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=12244
+var accountEmail="darshan@gmail.com"
+let accountPassword="5734354"
+accountCity="Jaipur"
+
+let accountDetails;
+//accountId=65
+accountEmail="canin@google.com"
+accountPassword="78098"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountDetails])
